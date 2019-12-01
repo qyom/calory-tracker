622e94abd835f1f828dc50d9606d6f2ea3c1f81b
Hayk Hakobyan <hayk15@yahoo.com>
2019-11-30 18:18:47 -0800

    Create JWT middleware and setup routes for controllers

--- server/routes/api.php
+++ server/routes/api.php
@@ -13,6 +13,15 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('member', 'MemberController@register');
+Route::post('auth', 'AuthController@post');
+Route::get('open', 'SomeController@open');
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::get('auth', 'AuthController@get');
+    Route::get('closed', 'SomeController@closed');
 });
