76c370f6673e392cc94308570b5e61296fecb507
Hayk Hakobyan <hayk15@yahoo.com>
2019-12-02 12:39:48 -0800

    Finalize MemberTest: Post+Put

--- server/tests/Api/MemberTest.php
+++ server/tests/Api/MemberTest.php
@@ -16,7 +16,7 @@ class MemberTest extends TestCase
         'password_confirmation' => 'toptaltest',
     ];
 
-    public function testInvalidPost()
+    public function testInvalidPostInput()
     {
         // Empty first_name
         $this->json(
@@ -24,21 +24,21 @@ class MemberTest extends TestCase
             '/api/member',
             ['first_name'=>''] + $this->validPayload
 
-        )->assertStatus(401)->assertJsonStructure(['first_name']);
+        )->assertStatus(400)->assertJsonStructure(['first_name']);
         // Long first_name
         $this->json(
             'post',
             '/api/member',
             ['first_name'=>str_pad("", 256, "what-a-long-name")] + $this->validPayload
 
-        )->assertStatus(401)->assertJsonStructure(['first_name']);
+        )->assertStatus(400)->assertJsonStructure(['first_name']);
         // Empty last_name
         $this->json(
             'post',
             '/api/member',
             ['last_name'=>''] + $this->validPayload
 
-        )->assertStatus(401)->assertJsonStructure(['last_name']);
+        )->assertStatus(400)->assertJsonStructure(['last_name']);
         // Long last_name
         $this->json(
             'post',
@@ -48,7 +48,7 @@ class MemberTest extends TestCase
                 'first_name'=>str_pad("", 255, "not-quite-long-first-name")
             ] + $this->validPayload
 
-        )->assertStatus(401)->assertJsonStructure(['last_name'])->assertJsonMissingValidationErrors(['first_name']);
+        )->assertStatus(400)->assertJsonStructure(['last_name'])->assertJsonMissingValidationErrors(['first_name']);
         // Invalid email
         $this->json(
             'post',
@@ -57,28 +57,28 @@ class MemberTest extends TestCase
                 'email'=>'#@%^%#$@#$@#.com'
             ] + $this->validPayload
 
-        )->assertStatus(401)->assertJsonStructure(['email']);
+        )->assertStatus(400)->assertJsonStructure(['email']);
         // Mismatching password_confirmation field
         $this->json(
             'post',
             '/api/member',
             ['password_confirmation'=>'toptaltes'] + $this->validPayload
 
-        )->assertStatus(401)->assertJsonStructure(['password']);
+        )->assertStatus(400)->assertJsonStructure(['password']);
         // Mismatching password field
         $this->json(
             'post',
             '/api/member',
             ['password'=>'optaltest'] + $this->validPayload
 
-        )->assertStatus(401)->assertJsonStructure(['password']);
+        )->assertStatus(400)->assertJsonStructure(['password']);
         // invalid calories
         $this->json(
             'post',
             '/api/member',
             ['max_calories_per_day'=>-1] + $this->validPayload
 
-        )->assertStatus(401)->assertJsonStructure(['max_calories_per_day']);
+        )->assertStatus(400)->assertJsonStructure(['max_calories_per_day']);
         // Duplicate email entries
         $member = factory(Member::class)->create([
             'email' => $this->validPayload['email'],
@@ -89,10 +89,10 @@ class MemberTest extends TestCase
             '/api/member',
             $this->validPayload
 
-        )->assertStatus(401)->assertJsonStructure(['email']);
+        )->assertStatus(400)->assertJsonStructure(['email']);
     }
 
-    public function testValidPost()
+    public function testValidPostInput()
     {
         $response = $this->json('post', '/api/member', $this->validPayload);
         $response
@@ -112,7 +112,7 @@ class MemberTest extends TestCase
             
     }
     
-    public function testRegularMemberCantCreateNewMember()
+    public function testInvalidPostAccess()
     {
         // Create a regular member
         $member = factory(Member::class)->create([
@@ -125,7 +125,7 @@ class MemberTest extends TestCase
         $this->withTokenHeader($token)->json('post', '/api/member', $this->validPayload)->assertStatus(403);
     }
 
-    public function testInvalidPostAccess()
+    public function testValidPostAccess()
     {
         // Create a regular member
         $member = factory(Member::class)->create([
@@ -141,7 +141,7 @@ class MemberTest extends TestCase
         $this->withTokenHeader($token)->json('post', '/api/member', ['email'=>'another@email.com']+$this->validPayload)->assertStatus(201);
     }
 
-    public function testInvalidPut()
+    public function testInvalidPutInput()
     {
         // Create a regular member
         $member = factory(Member::class)->create([
@@ -149,7 +149,7 @@ class MemberTest extends TestCase
             'role_type' => Member::TYPE_REGULAR,
             'password' => bcrypt('toptaltest'),
         ]);
-        $token = JWTAuth::fromUser($member);
+        $tokenRegular = JWTAuth::fromUser($member);
         // Should reject unsigned but valid request
         $this->json(
             'put',
@@ -162,32 +162,30 @@ class MemberTest extends TestCase
             'put',
             '/api/member',
             ['first_name'=>''] + $this->validPayload
-        )->assertStatus(401)->assertJsonStructure(['first_name']);
+        )->assertStatus(400)->assertJsonStructure(['first_name']);
 
         // Long first_name
         $this->withTokenHeader($tokenRegular)->json(
             'put',
             '/api/member/'.$member->member_id,
             ['first_name'=>str_pad("", 256, "what-a-long-name")] + $this->validPayload
-        )->assertStatus(401)->assertJsonStructure(['first_name']);
+        )->assertStatus(400)->assertJsonStructure(['first_name']);
 
         // Empty last_name
         $this->withTokenHeader($tokenRegular)->json(
             'put',
             '/api/member',
             ['last_name'=>''] + $this->validPayload
-        )->assertStatus(401)->assertJsonStructure(['last_name']);
+        )->assertStatus(400)->assertJsonStructure(['last_name']);
         // Long last_name
         $this->withTokenHeader($tokenRegular)->json(
             'put',
             '/api/member/'.$member->member_id,
             [
-                'last_name'=>str_pad("", 256, "what-a-long-last-name"),
+                'last_name'=>str_pad("", 259, "what-a-long-last-name"),
                 'first_name'=>str_pad("", 255, "not-quite-long-first-name")
             ] + $this->validPayload
-
-        )->assertStatus(401)->assertJsonStructure(['last_name'])->assertJsonMissingValidationErrors(['first_name']);
-        
+        )->assertStatus(400)->assertJsonStructure(['last_name'])->assertJsonMissingValidationErrors(['first_name']);
         // Invalid email
         $this->withTokenHeader($tokenRegular)->json(
             'put',
@@ -195,27 +193,27 @@ class MemberTest extends TestCase
             [
                 'email'=>'#@%^%#$@#$@#.com'
             ] + $this->validPayload
-        )->assertStatus(401)->assertJsonStructure(['email']);
+        )->assertStatus(400)->assertJsonStructure(['email']);
         
         // Mismatching password_confirmation field
         $this->withTokenHeader($tokenRegular)->json(
             'put',
             '/api/member',
             ['password_confirmation'=>'toptaltes'] + $this->validPayload
-        )->assertStatus(401)->assertJsonStructure(['password']);
+        )->assertStatus(400)->assertJsonStructure(['password']);
         
         // Mismatching password field
         $this->withTokenHeader($tokenRegular)->json(
             'put',
             '/api/member',
             ['password'=>'optaltest'] + $this->validPayload
-        )->assertStatus(401)->assertJsonStructure(['password']);
+        )->assertStatus(400)->assertJsonStructure(['password']);
         // invalid calories
         $this->withTokenHeader($tokenRegular)->json(
             'put',
             '/api/member',
             ['max_calories_per_day'=>-1] + $this->validPayload
-        )->assertStatus(401)->assertJsonStructure(['max_calories_per_day']);
+        )->assertStatus(400)->assertJsonStructure(['max_calories_per_day']);
         // Duplicate email entries
         $member = factory(Member::class)->create([
             'email' => $this->validPayload['email'],
@@ -225,7 +223,7 @@ class MemberTest extends TestCase
             'put',
             '/api/member',
             $this->validPayload
-        )->assertStatus(401)->assertJsonStructure(['email']);
+        )->assertStatus(400)->assertJsonStructure(['email']);
     }
 
     public function testPutAccess()
@@ -239,42 +237,59 @@ class MemberTest extends TestCase
         // Generate its token to specify as current in header
         $token = JWTAuth::fromUser($currentMember);
 
-        // Attempt to update a non existing member should return 403
-        $this->withTokenHeader($token)->json(
+        // Attempt to update a non existing member should return 404
+        $response = $this->withTokenHeader($token)->json(
             'put',
-            '/api/member/'.($currentMember->member_id + 1),
+            '/api/member/'.($currentMember->member_id + 100),
             $this->validPayload
-        )->assertStatus(403);
+        );
+        //$response->dump();
+        $response->assertStatus(404);
         // Attempt to update an existing member other than self should return 403
-        $memberToUpdate = factory(Member::class)->create($this->validPayload);
+        $memberToUpdate = factory(Member::class)->create([
+            'email' => 'toptal2@test.com',
+            'role_type' => Member::TYPE_REGULAR,
+            'password' => bcrypt('toptaltest'),
+        ]);
         $this->withTokenHeader($token)->json(
             'put',
-            '/api/member'.$memberToUpdate->member_id,
+            '/api/member/'.$memberToUpdate->member_id,
             $this->validPayload
         )->assertStatus(403);
 
         // Change to Manager
         $currentMember->role_type = Member::TYPE_MANAGER;
         $currentMember->save();
+        // Refresh token
+        $token = $this->getToken();
         // Retry even with an old member token
-        $this->withTokenHeader($token)->json(
+        $response = $this->withTokenHeader($token)->json(
             'put',
-            '/api/member'.$memberToUpdate->member_id,
-            ['first_name'=>'changedByManager']+$this->validPayload
-        )->assertStatus(200);
+            '/api/member/'.$memberToUpdate->member_id,
+            ['first_name'=>'changedByManager', 'dump'=>1]+$this->validPayload
+        );
+        $response->assertStatus(200);
         $memberToUpdate->refresh();
-        $this->assertEqual('changedByManager', $memberToUpdate->first_name);
+        $this->assertEquals('changedByManager', $memberToUpdate->first_name);
 
         // Change to Admin
         $currentMember->role_type = Member::TYPE_MANAGER;
         $currentMember->save();
+        // Refresh token
+        $token = $this->getToken();
         // Retry even with an old member token
         $this->withTokenHeader($token)->json(
             'put',
-            '/api/member'.$memberToUpdate->member_id,
+            '/api/member/'.$memberToUpdate->member_id,
             ['first_name'=>'changedByAdmin'] + $this->validPayload
         )->assertStatus(200);
         $memberToUpdate->refresh();
-        $this->assertEqual('changedByAdmin', $memberToUpdate->first_name);         
+        $this->assertEquals('changedByAdmin', $memberToUpdate->first_name);         
+    }
+
+    protected function getToken()
+    {
+        JWTAuth::parseToken()->authenticate();
+        return JWTAuth::getToken();
     }
 }
