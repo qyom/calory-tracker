0b03a0bd3118e310482b48a3b76c3c4a46387a85
Hayk Hakobyan <hayk15@yahoo.com>
2019-12-10 22:59:24 -0800

    Server: fix cores for all request types

--- server/app/Http/Middleware/Cors.php
+++ server/app/Http/Middleware/Cors.php
@@ -5,13 +5,10 @@ class Cors
 {
   public function handle($request, Closure $next)
   {
-    if ($request->getMethod() === "OPTIONS") {
-        return response('', 204)
-          ->header('Access-Control-Allow-Origin', '*')
-          ->header('Vary', 'Origin, Access-Control-Request-Headers')
-          ->header('Access-Control-Allow-Methods', 'GET, HEAD, POST, PATCH, PUT, DELETE, OPTIONS')
-          ->header('Access-Control-Allow-Headers', 'content-type');
-    }
-    return $next($request);
+    return  $next($request)
+      ->header('Access-Control-Allow-Origin', '*')
+      ->header('Vary', 'Origin, Access-Control-Request-Headers')
+      ->header('Access-Control-Allow-Methods', 'GET, HEAD, POST, PATCH, PUT, DELETE, OPTIONS')
+      ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
   }
 }
